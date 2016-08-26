d006e15567150011142d425938ae74252176159a
EnigmaSn <enigmasnape@gmail.com>
2016-08-26 12:41:52 +0300

    Подключила JS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var button = document.querySelector(".search-but");
+var popup = document.querySelector(".search-hotels");
+
+button.addEventListener("click", function(event) {
+    if (popup.classList.contains("search-hotels-show")) {
+        event.preventDefault();
+        popup.classList.remove("search-hotels-show");
+    } 
+    else {
+        event.preventDefault();
+        popup.classList.add("search-hotels-show");
+    }
+});
